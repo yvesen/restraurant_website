66270fd4ba533dc084b5da7bfdd1e51537147553
yvesen <l.sen.t@hotmail.com>
2017-12-08 14:22:54 +0800

    add category index page for admin

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 	
 	namespace :admin do
 		resources :restaurants
-    resources :category
+    resources :categories
     root "restaurants#index"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
