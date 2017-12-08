4eb3150c5f5203f394d3dcd97b64ec7463ff1dc3
yvesen <l.sen.t@hotmail.com>
2017-12-08 13:43:57 +0800

    setup route resources for admin categories

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	
 	namespace :admin do
 		resources :restaurants
+    resources :category
     root "restaurants#index"
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
