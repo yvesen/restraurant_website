7f71040d90149c3b63d1ce4747b860c414d21bf1
yvesen <l.sen.t@hotmail.com>
2017-12-11 00:06:50 +0800

    move authenticate_user! to application_controller.rb

--- app/controllers/admin/restaurants_controller.rb
+++ app/controllers/admin/restaurants_controller.rb
@@ -1,5 +1,4 @@
 class Admin::RestaurantsController < ApplicationController
-	before_action :authenticate_user!
   before_action :authenticate_admin
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
