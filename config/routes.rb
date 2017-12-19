24d9b2e97036f046c12917c1bea86b78bbb70cd4
yvesen <l.sen.t@hotmail.com>
2017-12-19 14:34:40 +0800

    setup comment resources for public users

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :restaurants, only: [:index, :show]
+  resources :restaurants, only: [:index, :show] do
+    resources :comments, only: [:create, :destroy]
+  end
+  
   resources :categories, only: :show
 	root "restaurants#index"
 	
