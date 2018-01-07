9f81ec6baceffd45bc471eaa1b66e1dd5f816358
yvesen <l.sen.t@hotmail.com>
2018-01-07 22:17:57 +0800

    add favorite / unfavorite action on RestaurantsController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -18,5 +18,18 @@ class RestaurantsController < ApplicationController
   def dashboard
     @restaurant = Restaurant.find(params[:id])
   end
+
+  def favorite
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.favorites.create!(user: current_user)
+    redirect_back(fallback_location: root_path)  # 導回上一頁   
+  end
+
+  def unfavorite
+    @restaurant = Restaurant.find(params[:id])
+    favorite = Favorite.where(restaurant: @restaurant, user: current_user)
+    favorite.destroy_all
+    redirect_back(fallback_location: root_path)
+  end
   
 end
