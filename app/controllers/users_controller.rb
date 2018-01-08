72263e39a68eb07c441ccd7a585a75ba3ae56f48
yvesen <l.sen.t@hotmail.com>
2018-01-08 22:48:20 +0800

    add user index page with follow button and validate uniqueness of following_id in user_id scope and add create action for followship record

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
 
   before_action :set_user,only:[:show, :edit, :update]
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
     @commented_restaurants = @user.restaurants.uniq
